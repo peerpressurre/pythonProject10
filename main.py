94c95fc35223a645d0c60749d7afd97987a06828
katet0k <katakostucenko0@gmail.com>
2022-10-15 12:36:51 +0300

    Update: hm1

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ try:
     for i in range(num1,num2):
         counter = 0
         if i == 1:
-            print(f'i, \t', end=' ')
+            print(f'{i}, \t', end=' ')
             continue
         for j in range(1,num2+1):
             if i % j == 0:
                counter+=1
         if counter == 2:
-            print(i, '\t', end=' ')
+            print(f'{i}, \t', end=' ')
 except Exception as ex:
     print(f'Error: {ex}')

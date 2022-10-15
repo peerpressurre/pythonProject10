f5f52f36f86732c1a95d78bb05650c8cf86b9f50
katet0k <katakostucenko0@gmail.com>
2022-10-15 12:07:56 +0300

    Update: feature/hm_ex1

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ try:
     num1 = int(input('->'))
     num2 = int(input('->'))
     list = []
-    for i in range(num1,num2):
-        for j in range(num1,num2):
-            if i % i == 0 and i % j != 0:
-               list.append(i)
+    counter = 0
 
-    print(f'{list}')
+    for i in range(num1,num2):
+        counter = 0
+        if i == 1:
+            print(f'i, \t', end=' ')
+            continue
+        for j in range(1,num2+1):
+            if i % j == 0:
+               counter+=1
+        if counter == 2:
+            print(i, '\t', end=' ')
 except Exception as ex:
     print(f'Error: {ex}')

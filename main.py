e8d1cb9e7bb79dd916025f4f0ed86d2a571d7a9b
katet0k <katakostucenko0@gmail.com>
2022-10-15 12:25:46 +0300

    Update: hm3

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ try:
    num1 = int(input('->'))
    num2 = int(input('->'))
 
-   for i in range(num1, num2):
-       for j in range(num1, num2):
-           print(f'{i} * {j} =', i * j, end="\t")
-       print("\n")
+   for i in range(1,10):
+       for j in range(num1, num2+1):
+           print(f'{j} * {i} =', i * j, end="\t\t")
+       print("")
 except Exception as ex:
     print(f'Error: {ex}')

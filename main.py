e382b3dc3416546d856b89a6fd3a1ee50d7f1c8f
katet0k <katakostucenko0@gmail.com>
2022-10-15 12:22:36 +0300

    Update: hm2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 try:
    for i in range(1, 10):
        for j in range(1, 10):
-           print(f'{i} * {j} =', i * j, end="\t")
-       print("\n")
+           print(f'{j} * {i} =', i * j, end="\t\t")
+       print("")
 except Exception as ex:
     print(f'Error: {ex}')

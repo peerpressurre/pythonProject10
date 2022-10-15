47deee62c808fd8eaee41f48f87547c012070f20
katet0k <katakostucenko0@gmail.com>
2022-10-15 12:15:48 +0300

    Update: feature/hm_ex3

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 try:
-   for i in range(1, 10):
-       for j in range(1, 10):
+   num1 = int(input('->'))
+   num2 = int(input('->'))
+
+   for i in range(num1, num2):
+       for j in range(num1, num2):
            print(f'{i} * {j} =', i * j, end="\t")
        print("\n")
 except Exception as ex:
-    print(f'Error: {ex}'
+    print(f'Error: {ex}')
